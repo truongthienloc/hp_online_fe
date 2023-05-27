46c40c7359409c6a8e252173739e71de228a456d
unknown <truongthienloc2@gmail.com>
2023-05-27 23:47:41 +0700

    fix

--- src/components/users/content.tsx
+++
@@ -1,9 +0,0 @@
-import Category from './[category]';
-const Content = () => {
-    return (
-        <div className="">
-            <Category />
-        </div>
-    );
-};
-export default Content;
